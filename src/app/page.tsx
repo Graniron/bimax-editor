4dce5d05de2c052be7daf47fab13fb9f4208c258
graniron <yavorco1991@gmail.com>
2026-01-17 18:35:43 +0100

    07: view projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import { Button } from "@/components/ui/button";
+import { ProjectsView } from "@/features/projects/components/projects-view";
 
-const Page = () => {
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
-
-  return <div>
-    <Button onClick={() => createProject({ name: 'New project' })}>Add new</Button>
-    {projects?.map((project) => (
-      <div key={project._id} className="mb-4 p-4 border rounded">
-        <h2 className="text-lg font-bold">{project.name}</h2>
-        <div>{project.ownerId}</div>
-      </div>
-    ))}
-  </div>;
+const Home = () => {
+  return <ProjectsView />
 };
 
-export default Page;
+export default Home;
